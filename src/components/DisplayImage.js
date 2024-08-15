56e58730ef7b9ef0f56932177bbd1002a1d2f295
abdulrahmanmundhera3637 <abdulrahmanmundhera@gmail.com>
2024-08-15 03:59:07 -0700

    Update2 DisplayImage.js
    Update2 DisplayImage.js

--- src/components/DisplayImage.js
+++ src/components/DisplayImage.js
@@ -4,7 +4,12 @@ import { IoMdClose } from 'react-icons/io';
 const DisplayImage = ({ imgUrl, onClose }) => {
   return (
     <div className='fixed inset-0 flex justify-center items-center bg-black bg-opacity-70 z-50'>
-      <div className='bg-white shadow-lg rounded max-w-[30vw] max-h-[70vh] relative p-4'>
+      <div className='bg-white shadow-lg rounded relative p-4 
+        max-w-[90vw] max-h-[90vh] 
+        sm:max-w-[80vw] sm:max-h-[80vh] 
+        md:max-w-[70vw] md:max-h-[70vh] 
+        lg:max-w-[50vw] lg:max-h-[50vh] 
+        xl:max-w-[40vw] xl:max-h-[60vh]'>
         <div
           className="absolute top-2 right-2 cursor-pointer text-2xl hover:text-red-600"
           onClick={onClose}
