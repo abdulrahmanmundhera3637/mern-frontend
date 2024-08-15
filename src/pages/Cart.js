09639b29c2aeb7d6f3e7d43115f7bbc74d3c580f
abdulrahmanmundhera3637 <abdulrahmanmundhera@gmail.com>
2024-08-15 03:14:51 -0700

    Update 2 Cart.js
    Update 2 Cart.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -109,15 +109,15 @@ const Cart = () => {
 
     return (
         <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
-            <div className='text-center text-lg my-3'>
+            <div className='text-center text-lg sm:text-xl my-3'>
                 {data.length === 0 && !loading && (
-                    <p className='bg-white py-5'>No Data</p>
+                    <p className='bg-white py-5 text-sm sm:text-base'>No Data</p>
                 )}
             </div>
 
-            <div className='flex flex-col gap-6 lg:flex-row lg:gap-10 lg:justify-between'>
+            <div className='flex flex-col gap-6 md:flex-row md:gap-8 lg:gap-10 lg:justify-between'>
                 {/* View product */}
-                <div className='w-full max-w-3xl'>
+                <div className='w-full md:w-2/3'>
                     {loading ? (
                         loadingCart.map((_, index) => (
                             <div key={index} className='w-full bg-slate-200 h-32 my-2 border border-slate-300 animate-pulse rounded'></div>
@@ -125,14 +125,14 @@ const Cart = () => {
                     ) : (
                         data.map((product) => (
                             <div key={product._id} className='w-full bg-white h-auto my-2 border border-slate-300 rounded grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[128px,1fr] gap-4'>
-                                <div className='w-full bg-slate-200'>
+                                <div className='w-full sm:w-32 h-32 bg-slate-200'>
                                     <img
                                         src={product.productId?.productImage?.[0] || '/path/to/default/image.jpg'}
                                         className='w-full h-full object-cover'
                                         alt={product.productId?.productName || 'Product Image'}
                                     />
                                 </div>
-                                <div className='px-4 py-2 relative'>
+                                <div className='px-2 py-2 sm:px-4 sm:py-4 relative'>
                                     {/* Delete product */}
                                     <div
                                         className='absolute top-2 right-2 text-red-600 rounded-full p-2 hover:bg-red-600 hover:text-white cursor-pointer'
@@ -141,22 +141,22 @@ const Cart = () => {
                                         <MdDelete />
                                     </div>
 
-                                    <h2 className='text-lg sm:text-xl text-ellipsis line-clamp-1'>{product.productId?.productName}</h2>
-                                    <p className='capitalize text-slate-500'>{product.productId?.category}</p>
-                                    <div className='flex items-center justify-between'>
-                                        <p className='text-red-600 font-medium text-lg'>{displayPKRCurrency(product.productId?.sellingPrice)}</p>
-                                        <p className='text-slate-600 font-semibold text-lg'>{displayPKRCurrency(product.productId?.sellingPrice * product.quantity)}</p>
+                                    <h2 className='text-base sm:text-lg lg:text-xl text-ellipsis line-clamp-1'>{product.productId?.productName}</h2>
+                                    <p className='capitalize text-slate-500 text-sm sm:text-base'>{product.productId?.category}</p>
+                                    <div className='flex items-center justify-between mt-2'>
+                                        <p className='text-red-600 font-medium text-sm sm:text-lg'>{displayPKRCurrency(product.productId?.sellingPrice)}</p>
+                                        <p className='text-slate-600 font-semibold text-sm sm:text-lg'>{displayPKRCurrency(product.productId?.sellingPrice * product.quantity)}</p>
                                     </div>
                                     <div className='flex items-center gap-2 mt-1'>
                                         <button
-                                            className='border border-red-600 text-red-600 hover:bg-red-600 hover:text-white w-8 h-8 flex justify-center items-center rounded'
+                                            className='border border-red-600 text-red-600 hover:bg-red-600 hover:text-white w-6 h-6 flex justify-center items-center rounded text-sm'
                                             onClick={() => decreaseQty(product._id, product.quantity)}
                                         >
                                             -
                                         </button>
-                                        <span>{product.quantity}</span>
+                                        <span className='text-sm sm:text-base'>{product.quantity}</span>
                                         <button
-                                            className='border border-red-600 text-red-600 hover:bg-red-600 hover:text-white w-8 h-8 flex justify-center items-center rounded'
+                                            className='border border-red-600 text-red-600 hover:bg-red-600 hover:text-white w-6 h-6 flex justify-center items-center rounded text-sm'
                                             onClick={() => increaseQty(product._id, product.quantity)}
                                         >
                                             +
@@ -169,23 +169,23 @@ const Cart = () => {
                 </div>
 
                 {/* Summary */}
-                <div className='w-full lg:max-w-sm'>
+                <div className='w-full md:w-1/3'>
                     {loading ? (
                         <div className='h-36 bg-slate-200 border border-slate-300 animate-pulse'></div>
                     ) : (
-                        <div className='h-auto bg-white p-4 rounded'>
-                            <h2 className='text-white bg-red-600 px-4 py-1 rounded-t'>Summary</h2>
-                            <div className='flex items-center justify-between mt-2 font-medium text-lg text-slate-600'>
+                        <div className='bg-white p-4 rounded shadow-md'>
+                            <h2 className='text-white bg-red-600 px-4 py-2 rounded-t text-base sm:text-lg'>Summary</h2>
+                            <div className='flex items-center justify-between mt-2 font-medium text-sm sm:text-base text-slate-600'>
                                 <p>Quantity</p>
                                 <p>{totalQty}</p>
                             </div>
 
-                            <div className='flex items-center justify-between mt-2 font-medium text-lg text-slate-600'>
+                            <div className='flex items-center justify-between mt-2 font-medium text-sm sm:text-base text-slate-600'>
                                 <p>Total Price</p>
                                 <p>{displayPKRCurrency(totalPrice)}</p>
                             </div>
 
-                            <button className='bg-blue-600 p-2 text-white w-full mt-4 rounded'>Payment</button>
+                            <button className='bg-blue-600 p-2 text-white w-full mt-4 rounded text-sm sm:text-base'>Payment</button>
                         </div>
                     )}
                 </div>
